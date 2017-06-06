20068b8e326e5a6fe8f8c9ca6ce1905388e33971
quanh <quanh206@gmail.com>
2017-06-06 15:02:19 -0700

    edited version

--- script_lab.sh
+++ script_lab.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-location=$pwd
+location=$( pwd )
+username=$USER
+userid=$( id -u $username )
+privileges=$( sudo -l | grep '(' )
+processes=$( ps -aux | grep $username | grep /usr/bin )
 
-echo "You are currently at $location"
-echo "Your user name is "
-echo "Your UID is "
-echo "Your privileges are "
-echo "Processes currently running "
+echo "You are currently at: $location"
+echo -e  "\nYour user name is: $username"
+echo -e  "\nYour UID is: $userid"
+echo -e "\nYour privileges are: $privileges"
+echo -e "\nProcesses currently running:"
+echo "$processes"
